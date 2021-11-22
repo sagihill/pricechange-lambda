9ae2a698428ab6b0b6ab5d50f4860a17847e23ad
Sagi Hillel <sagi@noosa.io>
2021-11-22 14:32:13 +0200

    'deploy'

--- index.ts
+++ index.ts
@@ -1,79 +1,69 @@
 const axios = require("axios").default;
 
-// type ClockHandlerEvent = {
-//   symbol: string;
-// };
+type PriceInsertEvent = {
+  id: number;
+  price: number;
+  symbol: string;
+};
 
-exports.handler = async (event: any) => {
+exports.handler = async (event: PriceInsertEvent) => {
   try {
     console.log("Started handling price change event: ", event);
-    // const price = await getPrice(event.symbol);
-    // const priceId = await insertPrice(event.symbol, price);
-    // console.log("Finished handling price change event...");
-    return true;
+    const lastPrice = await getLastPrice(event.id, event.symbol);
+    const priceChange = round((event.price - lastPrice) / 100);
+    console.log(`Current price change is ${priceChange}`);
+    const priceChangeId = await insertPriceChange(event.symbol, priceChange);
+    console.log("Finished handling price change event...");
+    return priceChangeId;
   } catch (error) {
     console.log("Something went wrong handling event", [error, event]);
   }
 };
 
-// async function getPrice(symbol: string): Promise<number> {
-//   console.log(`Started fetching price for "${symbol}"`);
-//   const options = {
-//     headers: {
-//       "x-api-key": "L5KARBpGqm6aQPrXPfwgl6E5Ild5pRBh8dG7cb6a",
-//     },
-//   };
+async function getLastPrice(id: number, symbol: string): Promise<number> {
+  console.log(`Getting last price of ${symbol}`);
+  const connection = {
+    ssl: { rejectUnauthorized: false },
+    host: "tradewatch-1.ckjhl9zn95xm.eu-central-1.rds.amazonaws.com",
+    port: "3306",
+    user: "admin",
+    password: "sagi1991",
+    database: "TradeWatch",
+  };
+  const knex = require("knex")({
+    client: require("knex/lib/dialects/mysql"),
+    connection,
+  });
 
-//   const resp = await axios.get(
-//     `https://yfapi.net/v7/finance/options/${symbol}`,
-//     options
-//   );
+  const res = await knex("Prices").where("id", id - 1);
+  console.log(`Last price of ${symbol} was ${res[0]}`);
+  return res[0];
+}
 
-//   const data: APIResponse = resp.data;
+async function insertPriceChange(
+  symbol: string,
+  priceChange: number
+): Promise<number> {
+  console.log("Inserting price change to database...");
+  const connection = {
+    ssl: { rejectUnauthorized: false },
+    host: "tradewatch-1.ckjhl9zn95xm.eu-central-1.rds.amazonaws.com",
+    port: "3306",
+    user: "admin",
+    password: "sagi1991",
+    database: "TradeWatch",
+  };
+  const knex = require("knex")({
+    client: require("knex/lib/dialects/mysql"),
+    connection,
+  });
 
-//   const price = data.optionChain.result[0].quote.regularMarketPrice;
+  const res = await knex("PriceChanges").insert({ symbol, priceChange });
+  console.log("Finished inserting price change into the database...");
+  return res[0];
+}
 
-//   console.log(`${symbol} price is: ${price}`);
-
-//   return price;
-// }
-
-// async function getLastPrice(symbol: string): Promise<number> {
-//   console.log("Inserting price to database...");
-//   const connection = {
-//     ssl: { rejectUnauthorized: false },
-//     host: "tradewatch-1.ckjhl9zn95xm.eu-central-1.rds.amazonaws.com",
-//     port: "3306",
-//     user: "admin",
-//     password: "sagi1991",
-//     database: "TradeWatch",
-//   };
-//   const knex = require("knex")({
-//     client: require("knex/lib/dialects/mysql"),
-//     connection,
-//   });
-
-//   const res = await knex("Prices").select;
-//   console.log("Finished inserting price into the database...");
-//   return res[0];
-// }
-
-// async function insertPrice(symbol: string, price: number): Promise<number> {
-//   console.log("Inserting price to database...");
-//   const connection = {
-//     ssl: { rejectUnauthorized: false },
-//     host: "tradewatch-1.ckjhl9zn95xm.eu-central-1.rds.amazonaws.com",
-//     port: "3306",
-//     user: "admin",
-//     password: "sagi1991",
-//     database: "TradeWatch",
-//   };
-//   const knex = require("knex")({
-//     client: require("knex/lib/dialects/mysql"),
-//     connection,
-//   });
-
-//   const res = await knex("Prices").insert({ symbol, price });
-//   console.log("Finished inserting price into the database...");
-//   return res[0];
-// }
+function round(num: number): number {
+  var m = Number((Math.abs(num) * 100).toPrecision(15));
+  return (Math.round(m) / 100) * Math.sign(num);
+}
