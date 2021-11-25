d709292155cd9cabe3223fb42765c045f80a5739
Sagi Hillel <sagi@noosa.io>
2021-11-25 11:02:08 +0200

    'deploy'

--- index.ts
+++ index.ts
@@ -6,6 +6,19 @@ type PriceInsertEvent = {
   symbol: string;
 };
 
+const connection = {
+  ssl: { rejectUnauthorized: false },
+  host: "tradewatch-2-instance-1.ckjhl9zn95xm.eu-central-1.rds.amazonaws.com",
+  port: "3306",
+  user: "admin",
+  password: "sagi1991",
+  database: "TradeWatch",
+};
+const knex = require("knex")({
+  client: require("knex/lib/dialects/mysql"),
+  connection,
+});
+
 exports.handler = async (event: PriceInsertEvent) => {
   try {
     console.log("Started handling price change event: ", event);
@@ -22,18 +35,6 @@ exports.handler = async (event: PriceInsertEvent) => {
 
 async function getLastPrice(id: number, symbol: string): Promise<number> {
   console.log(`Getting last price of ${symbol}`);
-  const connection = {
-    ssl: { rejectUnauthorized: false },
-    host: "tradewatch-1.ckjhl9zn95xm.eu-central-1.rds.amazonaws.com",
-    port: "3306",
-    user: "admin",
-    password: "sagi1991",
-    database: "TradeWatch",
-  };
-  const knex = require("knex")({
-    client: require("knex/lib/dialects/mysql"),
-    connection,
-  });
 
   const res = await knex("Prices").where("id", id - 1);
   const lastPrice = res[0].price;
@@ -46,27 +47,9 @@ async function insertPriceChange(
   priceChange: number
 ): Promise<number> {
   console.log("Inserting price change to database...");
-  const connection = {
-    ssl: { rejectUnauthorized: false },
-    host: "tradewatch-2-instance-1.ckjhl9zn95xm.eu-central-1.rds.amazonaws.com",
-    port: "3306",
-    user: "admin",
-    password: "sagi1991",
-    database: "TradeWatch",
-  };
-  const knex = require("knex")({
-    client: require("knex/lib/dialects/mysql"),
-    connection,
-  });
-
-  const res = await knex("PriceChanges").select();
-
-  console.log(JSON.stringify(res));
+  const res = await knex("PriceChanges").insert({ symbol, priceChange });
+  console.log("Finished inserting price change into the database...");
   return res[0];
-
-  // const res = await knex("PriceChanges").insert({ symbol, price: priceChange });
-  // console.log("Finished inserting price change into the database...");
-  // return res[0];
 }
 
 function round(num: number): number {
